27d8a321addc36ec82075d4a3ef19a1d84ee12d2
Liam Stabeno <Liamstabeno@gmail.com>
2017-04-25 15:18:19 -0700

    start to introduce api, not working***

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,1 +1,14 @@
 var Doctor = require('../js/doctor.js').doctorModule;
+
+function showDoctors()
+
+$(document).ready(function() {
+  var currentWeatherObject = new Weather();
+  $('#weather-location').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    currentWeatherObject.getWeather(city, displayHumidity);
+    console.log("trying to display humidity front-end");
+    console.log(currentWeatherObject);
+  });
+});
--- js/doctor.js
+++ js/doctor.js
@@ -4,16 +4,15 @@ var apiKey = require('./../.env').apiKey;
 function Doctor() {
 }
 
-Doctor.prototype.getDoctor = function () {
-  console.log("Started the doctor search");
-$.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
- .then(function(result) {
-    console.log(result);
-  })
- .fail(function(error){
-    console.log("fail");
-  });
-};
+// Doctor.prototype.getDoctor = function (name, location) {
+//   console.log("Started the doctor search");
+// $.get('https://api.betterdoctor.com/2016-03-01/doctors?' + name + 'location=' + location + '%2C100&user_location=' + location + '&sort=distance-asc&skip=0&limit=10&user_key=' + apiKey).then(function(response) {
+//     console.log(response);
+//   })
+//  .fail(function(error){
+//     console.log("fail");
+//   });
+// };  SO BROKEN 
 
 
 exports.doctorModule = Doctor;
