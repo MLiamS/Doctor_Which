257a2c4a98b332632898ccd4d6fd0917416cdda8
Liam Stabeno <liamstabeno@gmail.com>
2017-05-08 09:53:53 -0700

    log the first and last name of the first doctor object

--- js/doctor.js
+++ js/doctor.js
@@ -3,16 +3,16 @@ var apiKey = require('./../.env').apiKey;
 Doctor = function(){};
 
 showDoctors = function(result) {
-  for (var i = 0; i < medicalIssue.length; i++) {
+  for (var i = 0; i < (result).length; i++) {
     console.log("Trying to display doctors");
-   $('#doctors').append('<li>' + medicalIssue[i] + '</li>');
+   $('#doctors').append('<li>' + result[i].data + '</li>');
   }
 };
 
 Doctor.prototype.getDoctors = function(medicalIssue, showDoctors) {
   console.log("Trying to find some doctors");
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(result) { console.log(result.data);
+   .then(function(result) { console.log(result.data[0].profile.first_name + ' ' + result.data[0].profile.last_name);
    })
    .fail(function(error){
       console.log("fail");
