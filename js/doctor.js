36ee4339b7d494ad25efbde7d424f7fff3c861ea
Liam Stabeno <liamstabeno@gmail.com>
2017-05-08 09:40:50 -0700

    display result.data

--- js/doctor.js
+++ js/doctor.js
@@ -4,7 +4,7 @@ Doctor = function(){};
 
 showDoctors = function(result) {
   for (var i = 0; i < medicalIssue.length; i++) {
-    console.log("trying to display doctors");
+    console.log("Trying to display doctors");
    $('#doctors').append('<li>' + medicalIssue[i] + '</li>');
   }
 };
@@ -12,8 +12,8 @@ showDoctors = function(result) {
 Doctor.prototype.getDoctors = function(medicalIssue, showDoctors) {
   console.log("Trying to find some doctors");
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(result) { showDoctors(result);
-       })
+   .then(function(result) { console.log(result.data);
+   })
    .fail(function(error){
       console.log("fail");
     });
