5a6f4470a684bc6dc2d3846b89dc3584c5aab0f0
Liam Stabeno <liamstabeno@gmail.com>
2017-05-08 10:17:45 -0700

    Populate list of doctors first and last name

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -2,16 +2,10 @@ var Doctor = require('../js/doctor.js').doctorModule;
 
 
 $(document).ready(function() {
-  console.log("help");
   var doctors = new Doctor();
   $('#queryForm').submit(function(event) {
     event.preventDefault();
-    console.log(medicalIssue);
-
     var medicalIssue = $('#medicalIssue').val();
-    console.log(medicalIssue);
     doctors.getDoctors(medicalIssue);
-
-    
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -2,17 +2,17 @@ var apiKey = require('./../.env').apiKey;
 
 Doctor = function(){};
 
-showDoctors = function(result) {
-  for (var i = 0; i < (result).length; i++) {
+showDoctors = function(result) { console.log((result.data).length);
+  for (var i = 0; i < ((result.data).length); i++) {
     console.log("Trying to display doctors");
-   $('#doctors').append('<li>' + result[i].data + '</li>');
+   $('#doctors').append('<li>' + result.data[i].profile.first_name + " " + result.data[i].profile.last_name + '</li>');
   }
 };
 
-Doctor.prototype.getDoctors = function(medicalIssue, showDoctors) {
+Doctor.prototype.getDoctors = function(medicalIssue) {
   console.log("Trying to find some doctors");
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(result) { console.log(result.data[0].profile.first_name + ' ' + result.data[0].profile.last_name);
+   .then(function(result) { console.log(result.data[0].profile.first_name); showDoctors(result);
    })
    .fail(function(error){
       console.log("fail");
